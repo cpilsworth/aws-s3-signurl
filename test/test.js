5beb8415cc823cdb98eda4d428528d36b17db29c
Chris Pilsworth <cpilsworth@gmail.com>
2015-11-24 23:43:36 +0000

    Added integration tests for signed/unsigned urls

--- test/test.js
+++ test/test.js
@@ -2,9 +2,24 @@ var Code = require('code');   // assertion library
 var Lab = require('lab');
 var signUrl = require('../signurl.js');
 var lab = exports.lab = Lab.script();
+var https = require('https');
 
-// lab.test('returns true when 1 + 1 equals 2', function (done) {
-//     Code.expect(1+1).to.equal(2);
-//     done();
-// });
 
+ lab.test('base url is provided', function (done) {
+     Code.expect(signUrl.signUrl('aws-s3-signurl', 'test-file.txt')).to.startWith('https://aws-s3-signurl.s3.amazonaws.com/test-file.txt');
+     done();
+ });
+
+lab.test('url is blocked without signature', function (done) {
+    https.get('https://aws-s3-signurl.s3.amazonaws.com/test-file.txt', function (res) {
+        Code.expect(res.statusCode).to.equal(403);
+        done();
+    });
+});
+
+lab.test('url is blocked without signature', function (done) {
+    https.get(signUrl.signUrl('aws-s3-signurl', 'test-file.txt'), function (res) {
+        Code.expect(res.statusCode).to.equal(200);
+        done();
+    });
+});
