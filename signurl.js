6b7ac69a46d3db8c7030f961683eddf52e330330
Chris Pilsworth <cpilsworth@gmail.com>
2015-11-24 23:43:06 +0000

    Return the url from the signUrl function

--- signurl.js
+++ signurl.js
@@ -3,23 +3,22 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
 function signUrl(bucket, key) {
-  s3.getSignedUrl('getObject', {Bucket: bucket, Key: key }, function (err, url) {
-    if (err) process.exit(-1);
+    var url = s3.getSignedUrl('getObject', {Bucket: bucket, Key: key});
     console.log(url);
-  });
+    return url;
 }
 
 function listObjects(params) {
-  s3.listObjects(params, function(err, data) {
-    if (err) process.exit(-1);
-    
-    for (var item of data.Contents) {
-      signUrl(params.Bucket, item.Key);
-    }
-  });
+    s3.listObjects(params, function (err, data) {
+        if (err) process.exit(-1);
+
+        for (var item of data.Contents) {
+            signUrl(params.Bucket, item.Key);
+        }
+    });
 }
 
 module.exports = {
-  signUrl: signUrl,
-  listObjects: listObjects
+    signUrl: signUrl,
+    listObjects: listObjects
 };
