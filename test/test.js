612ca22e031e7dab4f3d440def98586f2723cbcd
Chris Pilsworth <cpilsworth@gmail.com>
2017-02-24 22:35:05 +0000

    Paramterised bucket/filename for test

--- test/test.js
+++ test/test.js
@@ -6,21 +6,26 @@ const lab = exports.lab = Lab.script()
 const https = require('https')
 const signUrl = require('../signurl.js')
 
+const testBucket = 'aws-s3-signurl-test';
+const testFile = 'test-file.txt';
+const testFileUrl = 'https://' + testBucket + '.s3.amazonaws.com/' + testFile;
+
 lab.test('base url is provided', (done) => {
-  Code.expect(signUrl.signUrl('aws-s3-signurl-test', 'test-file.txt')).to.startWith('https://aws-s3-signurl-test.s3.amazonaws.com/test-file.txt')
-  done()
-})
+  Code.expect(signUrl.signUrl(testBucket, 'test-file.txt')).to.startWith(testFileUrl)
+  done();
+});
 
 lab.test('url is blocked without signature', (done) => {
-  https.get('https://aws-s3-signurl-test.s3.amazonaws.com/test-file.txt', (res) => {
+  https.get(testFileUrl, (res) => {
     Code.expect(res.statusCode).to.equal(403)
     done()
-  })
-})
+  });
+});
 
-lab.test('url is blocked without signature', (done) => {
-  https.get(signUrl.signUrl('aws-s3-signurl', 'test-file.txt'), (res) => {
+lab.test('url is not blocked with signature', (done) => {
+  console.log(signUrl.signUrl(testBucket, testFile));
+  var req = https.get(signUrl.signUrl(testBucket, testFile), (res) => {
     Code.expect(res.statusCode).to.equal(200)
     done()
-  })
+  });
 })
